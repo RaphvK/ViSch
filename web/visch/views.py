143bfcef47d26f1dcabfd1d209e3c6b8df28e0b9
Nicolas <nico-busch@gmx.de>
2020-03-21 16:00:48 +0100

    addRegisterForm

--- web/visch/views.py
+++ web/visch/views.py
@@ -1,6 +1,40 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 # Create your views here.
+from django.urls import reverse
+
+
 def index(request):
     return render(request, 'visch/index.html')
+
+def register_view(request):
+    register_form = UserCreationForm()
+    if request.method == "POST":
+        register_form = UserCreationForm(request.POST)
+        if register_form.is_valid():
+            register_form.save()
+            username = register_form.cleaned_data['username']
+            password = register_form.cleaned_data['password1']
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)
+    return render(request, 'visch/register.html', {'register_form': register_form})
+
+def login_view(request):
+    login_form = AuthenticationForm()
+    if request.method == "POST":
+        login_form = AuthenticationForm(data=request.POST)
+        if login_form.is_valid():
+            username = login_form.cleaned_data['username']
+            password = login_form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)
+    return render(request, 'visch/login.html', {'login_form': login_form})
+
+def logout_view(request):
+    logout(request)
+    return redirect(reverse('web:index'))
