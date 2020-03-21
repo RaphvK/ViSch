cc66f9be1ee32fde045228cd46af516101d982d6
RaphvK <r.van-kempen@live.de>
2020-03-21 14:26:57 +0100

    integrated bootstrap template

--- web/web/settings.py
+++ web/web/settings.py
@@ -115,7 +115,13 @@ USE_L10N = True
 USE_TZ = True
 
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Additional locations of static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "visch", "static"),
+]
