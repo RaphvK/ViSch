1a1ab908c903715a6c1adf431b3ed1a041d393df
Nicolas <nico-busch@gmx.de>
2020-03-21 22:05:42 +0100

    logout

--- web/visch/models.py
+++ web/visch/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from django.utils import timezone
+
+
+class BlogPost(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.CharField(max_length=255)
+    created_at = models.DateTimeField(default=timezone.now)
